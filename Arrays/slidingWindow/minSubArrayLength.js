6deb2700995d93df02c5df7cc1ea1474fa56a5cb
Eric Thorfinnson <ethorf@gmail.com>
2020-08-27 10:30:48 -0400

    bruted my way through minSubArrayLength

--- Arrays/slidingWindow/minSubArrayLength.js
+++ Arrays/slidingWindow/minSubArrayLength.js
@@ -50,8 +50,73 @@ const minSubArrayLength2 = (arr, targetSum) => {
 
 	return tempArray;
 };
+//Okay so describing this as far as what I want to do with it.
+//I want to start the array by checking the first two elements and summing them to the temp sum
+//if the temp sum is less than the target sum, I will increment the right window and add that to the temp sum
+const minSubArrayLength3 = (arr, targetSum) => {
+	let leftWindow = 0;
+	let tempArray = [arr[0], arr[1]];
+	let lengthArray = [];
+	let tempSum = arr[0] + arr[1];
+
+	if (arr.slice().sort((a, b) => b - a)[0] >= targetSum) return 1;
+
+	for (let rightWindow = 1; rightWindow < arr.length; ) {
+		if (tempSum < targetSum) {
+			rightWindow++;
+			if (arr[rightWindow] !== undefined) {
+				tempArray.push(arr[rightWindow]);
+			}
+			tempSum += arr[rightWindow];
+		} else if (tempSum >= targetSum) {
+			tempArray.shift();
+			if (tempArray.reduce((a, b) => a + b) >= targetSum) {
+				lengthArray.push(tempArray.length);
+			}
+			leftWindow++;
+			tempSum -= arr[leftWindow];
+		}
+	}
+	if (tempArray.reduce((a, b) => a + b) < targetSum) {
+		return 0;
+	}
+	return lengthArray.sort((a, b) => a - b)[0];
+};
+
+///Below is the solution from the course
+function minSubArrayLength4(arr, sum) {
+	let total = 0;
+	let leftWindow = 0;
+	let rightWindow = 0;
+	let minLen = Infinity;
+
+	//Okay so the while was a good idea but I needed a break
+	while (leftWindow < arr.length) {
+		// if current window doesn't add up to the given sum then
+		// move the window to right
+		//Okay so this initialized them to 0 and just added the rightWindow to the sum initially
+		if (total < sum && rightWindow < arr.length) {
+			total += arr[rightWindow];
+			rightWindow++;
+		}
+		// if current window adds up to at least the sum given then
+		// we can shrink the window
+		else if (total >= sum) {
+			//Okay this thing I'll need to think of a little bit
+			minLen = Math.min(minLen, rightWindow - leftWindow);
+			total -= arr[leftWindow];
+			leftWindow++;
+		}
+		// current total less than required total but we reach the rightWindow, need this or else we'll be in an infinite loop
+		else {
+			break;
+		}
+	}
+
+	return minLen === Infinity ? 0 : minLen;
+}
 
 let arr1 = [2, 3, 1, 2, 4, 3]; //target = 7 /// 2 because [4,3] is the smallest subarray
 let arr2 = [2, 1, 6, 5, 4]; // target 9 /// 2 because [5,4] is the smallest subarray
-
-console.log(minSubArrayLength2(arr1, 7));
+let arr3 = [1, 4, 16, 22, 5, 7, 8, 9, 10]; // target 95
+console.log(minSubArrayLength3(arr1, 7));
