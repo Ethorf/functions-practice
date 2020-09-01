673581fb8fd2d994dc02714a82f9011a9127af2d
Eric Thorfinnson <ethorf@gmail.com>
2020-09-01 10:49:51 -0400

    working on some recursion

--- Recursion/fibonacci.js
+++ Recursion/fibonacci.js
@@ -4,7 +4,26 @@
 // fib(35) // 9227465
 //This function will return the nth number in the sequence
 
-const sumRange = (num) => {
-	if (num === 1) return 1;
-	return num + sumRange(num - 1);
+const fibonacci = (sequenceNum) => {
+	let sum = 1;
+	if (sequenceNum === 0) return sum;
+	sum = sum + sum;
+	console.log(`sum is currently:${sum}`);
+	return sum + fibonacci(sequenceNum - 1);
 };
+
+//fib we will decrease
+
+function fibonacci2(n) {
+	if (n <= 2) return 1;
+	// console.log(`n is currently:${n}`);
+	return fibonacci2(n - 1) + fibonacci2(n - 2);
+}
+console.log(fibonacci2(10));
+
+/// so for 10, we start out returning two recursions, this shit is blowing my mind
+//Thinking about recursion from the bottom up is apparently v helpful??
+
+//But where does this stuff return? what is the actual process here??
+//okay so these break out into little double chunks of themselves, each single branch spawns 2 more
+//The return has an implicit summing function which kind of starts
